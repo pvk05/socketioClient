e0bbe08044e2d01c7f6109e3db56a9a8f215d774
pvk05 <philip.v.knudsen@gmail.com>
2024-01-25 13:17:33 +0100

    Update connection URL in socket.js

--- public/socket.js
+++ public/socket.js
@@ -1,7 +1,7 @@
 console.log("socket.js loaded");
 var socketStatus = false
 document.getElementById("status").innerHTML = socketStatus;
-const connectionURL = "https://socketioserver-dev-mbea.1.ie-1.fl0.io";
+const connectionURL = process.env.CONNECTION_URL || "localhost:3000";
 console.log(connectionURL);
 
 const socket = io(connectionURL); // Replace with your server URL
