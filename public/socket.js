0ecd8deffa7e1d18691c04e41cee8eaabdc916f7
pvk05 <philip.v.knudsen@gmail.com>
2024-01-25 14:24:19 +0100

    Add username functionality and message object

--- public/socket.js
+++ public/socket.js
@@ -20,18 +20,36 @@ socket.on('disconnect', () => {
     document.getElementById("status").innerHTML = socketStatus;
 });
 
+var username = "user" + Math.floor(Math.random() * 1000);
+document.getElementById("username").innerHTML = username;
+
+document.getElementById("setUsername").addEventListener("click", () => {
+    username = document.getElementById("usernameInp").value;
+    document.getElementById("username").innerHTML = username;
+    document.getElementById("usernameInp").value = "";
+});
+
+class Message {
+    constructor(user, text) {
+        this.user = user;
+        this.text = text;
+
+    }
+}
+
 // Handle incoming events from the server
 socket.on('message', (data) => {
     console.log('Received data:', data);
     // Handle the received data
-    document.getElementById("messages").innerHTML += `<li>${data}</li>`;
+    document.getElementById("messages").innerHTML += `<li><strong>${data.user}</strong>: ${data.text}</li>`;
 });
 
 // Emit events to the server
 //socket.emit('eventName', { key: 'value' });
 
 document.getElementById("send").addEventListener("click", () => {
-    let msg = document.getElementById("messageInp").value;
-    socket.emit("message", msg);
+    let msgTxt = document.getElementById("messageInp").value;
+    let message = new Message(username, msgTxt);
+    socket.emit("message", message);
     document.getElementById("messageInp").value = "";
 });
